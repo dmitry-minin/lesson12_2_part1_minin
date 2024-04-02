124b82d93036e7c219d609bea5d31ec51a3ab1c9
Dmitry Minin <d.n.minin@gmail.com>
2024-04-02 20:26:51 +0200

    added new feature dicts and tests for it

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,4 +1,4 @@
-from utils import arrs
+from utils import arrs, dicts
 
 
 def test_get():
@@ -11,3 +11,13 @@ def test_slice():
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
     assert arrs.my_slice([], 1, 3) == []
     assert arrs.my_slice([1, 2, 3, 4]) == [1, 2, 3, 4]
+
+
+def test_get_val():
+    assert dicts.get_val({'a': 1, 'b': 2}, 'a') == 1
+    assert dicts.get_val({'a': 1, 'b': 2}, 'c', 'test') == 'test'
+    assert dicts.get_val({'a': 1, 'b': 2}, 'a', 'test') == 1
+    assert dicts.get_val({}, 'c', 'test') == 'test'
+
+
+
